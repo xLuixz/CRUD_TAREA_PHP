6659d55565fead8ed0098181d2d22e1ee977391e
xLuixz <xluigoodx@gmail.com>
2026-02-22 14:15:03 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,16 +1,6 @@
 <?php
-
 function conexion(){
-    $host="localhost";
-    $user="root";
-    $pass="";
-
-    $bd="prueba";
-
     $connect= mysqli_connect("localhost","root","","prueba");
-    mysqli_select_db($connect,$bd);
-
     return $connect;
 }
-
 ?>
